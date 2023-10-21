b0ad4acd1a32356c10879a1dd4b23cf5821c29a6
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2023-10-21 19:23:29 -0400

    s

--- pages/Upload.py
+++ pages/Upload.py
@@ -1,18 +1,11 @@
 import streamlit as st
+from deta import Deta
 
-st.write("#Upload a reciept")
+st.write("# Upload a receipt")
 
+# Initialize a streamlit file uploader widget.
 uploaded_file = st.file_uploader("Choose a file")
+
+# If user attempts to upload a file.
 if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
-
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
-
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
-
+    st.image(uploaded_file)
