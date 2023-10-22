e32c93e8cde76a0fc4b2c01831a892fd352d1333
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2023-10-21 23:28:39 -0400

    Update Upload.py

--- pages/Upload.py
+++ pages/Upload.py
@@ -36,3 +36,12 @@ def ocr(image):
     return text
 
 st.title("# Upload")
+
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
+if uploaded_file is not None:
+    st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
+    st.write("")
+    st.write("Classifying...")
+    # Perform OCR
+    text = ocr(uploaded_file)
+    st.write(f"**Extracted Text:**\n\n{text}")
