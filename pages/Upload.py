419ec812e5c7d5b316c1d9d294fbcd3bf65299fa
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2023-10-21 22:51:24 -0400

    ddddd

--- pages/Upload.py
+++ pages/Upload.py
@@ -1,14 +1,35 @@
+import cv2
 import streamlit as st
 from PIL import Image
 import pytesseract
+def preprocess_image(image):
+    # Convert to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Thresholding
+    _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    # Noise Reduction
+    denoised = cv2.fastNlMeansDenoising(thresh, None, 10, 7, 21)
+
+    # Contrast Enhancement
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    enhanced = clahe.apply(denoised)
+
+    return enhanced
 
 def ocr(image):
+    # Open the image
     if isinstance(image, str):
-        image = Image.open(image)
+        image = cv2.imread(image)
     else:
-        image = Image.open(image)
+        image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
 
-    text = pytesseract.image_to_string(image)
+    # Preprocess the image
+    preprocessed_image = preprocess_image(image)
+
+    # Perform OCR
+    text = pytesseract.image_to_string(preprocessed_image)
     return text
 
 st.title("# Upload")
