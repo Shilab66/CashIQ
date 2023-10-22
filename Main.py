564dae86f5468f1ebe1ee6754a47b665fd1b5203
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2023-10-22 02:17:06 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,34 @@
 import streamlit as st
-import pandas as pd
+import plotly.express as px
 
 # Define the path to the JSON file
-file_path = 'data.json'
+datafile = 'data.json'
+storeList = {}
+categoryList = {}
 
-# Load the JSON data into a pandas DataFrame
-df = pd.read_json(file_path)
+for key, value in datafile.items():
+    store = value["store"]
+    purchases = value["purchases"]
 
-st.dataframe(df)
+    if(store in storeList):
+        storeList[store] += 1;
+    else:
+        storeList[store] = 1;
+
+    for i in purchases:
+        category = i["category"];
+
+        if(category in categoryList):
+            categoryList[category] += 1;
+        else:
+            categoryList[category] = 1;
+
+
+col1A, col2A = st.columns(2)
+
+with col1A:
+    st.plotly_chart(px.pie(names=list(categoryList.keys()), values=list(categoryList.values()), title='Category'))
+with col2A:
+    max_value = max(categoryList.values())
+    max_key = [key for key, value in categoryList.items() if value == max_value]
+    st.write("Most Common Category: " + max_key)
