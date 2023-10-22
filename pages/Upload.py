f3d99433e3916e2e2c412dab86f3a63d430fc3d3
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2023-10-21 23:26:17 -0400

    redo

--- pages/Upload.py
+++ pages/Upload.py
@@ -1,6 +1,6 @@
 import cv2
 import streamlit as st
-import numpy as np
+from PIL import Image
 import pytesseract
 def preprocess_image(image):
     # Convert to grayscale
@@ -21,31 +21,18 @@ def preprocess_image(image):
 def ocr(image):
     # Open the image
     if isinstance(image, str):
+        image = Image.open(image)
         image = cv2.imread(image)
     else:
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        image = Image.open(image)
+        image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
 
     # Preprocess the image
     preprocessed_image = preprocess_image(image)
 
+    text = pytesseract.image_to_string(image)
     # Perform OCR
     text = pytesseract.image_to_string(preprocessed_image)
     return text
 
 st.title("# Upload")
-
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
-
-if uploaded_file is not None:
-    st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
-    st.write("")
-    st.write("Classifying...")
-
-    # Perform OCR
-    text = ocr(uploaded_file)
-
-    st.write(f"**Extracted Text:**\n\n{text}")
-
-    input_string = "How can I write a Python function?"
-    output = chat_with_gpt("given the raw text information from a reciept, could you seperaterate purchases into groups and recognize the store the its purchased from in a json file format. absolutely, always, please make sure that the json file only includes the purchases, with NO additional info. Only have a json file, no other words.")
-    st.write(output)
