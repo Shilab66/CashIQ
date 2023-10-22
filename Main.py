411cfbbfe373e1628ed00acec47af606d7ac1631
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2023-10-22 02:22:07 -0400

    fix json error

--- Main.py
+++ Main.py
@@ -1,34 +1,41 @@
 import streamlit as st
 import plotly.express as px
+import json
 
 # Define the path to the JSON file
 datafile = 'data.json'
+
+# Read the data from the JSON file
+with open(datafile, 'r') as file:
+    data = json.load(file)
+
 storeList = {}
 categoryList = {}
 
-for key, value in datafile.items():
+for value in data.values():  # Iterate over the values of the dictionary
     store = value["store"]
     purchases = value["purchases"]
 
-    if(store in storeList):
-        storeList[store] += 1;
+    if store in storeList:
+        storeList[store] += 1
     else:
-        storeList[store] = 1;
+        storeList[store] = 1
 
     for i in purchases:
-        category = i["category"];
+        category = i["category"]
 
-        if(category in categoryList):
-            categoryList[category] += 1;
+        if category in categoryList:
+            categoryList[category] += 1
         else:
-            categoryList[category] = 1;
-
+            categoryList[category] = 1
 
 col1A, col2A = st.columns(2)
 
 with col1A:
     st.plotly_chart(px.pie(names=list(categoryList.keys()), values=list(categoryList.values()), title='Category'))
+
 with col2A:
     max_value = max(categoryList.values())
     max_key = [key for key, value in categoryList.items() if value == max_value]
-    st.write("Most Common Category: " + max_key)
+    st.write("Most Common Category: " + max_key[0])  # Access the first element
+
